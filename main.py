385eff33d0bd83bacd824fcd4cef576161d84b6b
heyliceeee <alicedias2002@hotmail.com>
2026-05-06 09:40:14 +0100

    ✨feat: create screen

---
+++ main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+# 1. create a snake body (3 blocks)
+# 2. move the snake (continuously)
+# 3. control the snake (using arrows)
+# 4. detect collision with food (after increase snake and create another food in random location)
+# 5. create a scoreboard (increase score when snake collide with food)
+# 6. detect collision with wall (game over)
+# 7. detect collision with tail (game over)
+
+screen.exitonclick()
